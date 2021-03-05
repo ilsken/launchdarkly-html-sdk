25329b99e9310febd776cb234f4920f093911c28
Christopher Tarquini <chris+github@tarq.io>
2021-03-05 22:38:17 +0000

    it works now

--- launchdarkly-html-sdk.js
+++ launchdarkly-html-sdk.js
@@ -1,22 +1,55 @@
 (function(){
 
     var jsSdkUrl = "https://unpkg.com/launchdarkly-js-client-sdk/dist/ldclient.min.js";
-    var activeElements = getActiveElements();
-    var client_key = getClientKey();
     loadLdJsSDK(jsSdkUrl, initSdk);
+    
+
 
     // Returns an array of all elements with ld-data-* attributes
-    function getActiveElements() {
-
+    function updateAllElements() {
+      console.log("HI")
+      // you need to use data-ld because you can't select by attribute prefix
+      document.querySelectorAll("[data-ld],[data-ld-class],[data-ld-text],[data-ld-width],[data-ld-height],[data-ld-src]").forEach(elem => updateElement(elem));
     }
-
-    // Returns the client key specified in a data-ld-clientkey attr
-    function getClientKey() {
-
+    
+    function updateElement(elem) {
+      console.log("I AM ELEM")
+      if(!window.ldclient) return;
+      const attrs = Object.keys(elem.dataset).filter(value => {
+        return value.startsWith('ld')
+      }).forEach(key => {
+        const attrName = key.substring(2).toLowerCase();
+        console.log("WHOOPS", key, elem.dataset);
+        const flagKey = elem.dataset[key];
+        console.log(attrName, flagKey)
+        const NO_RESULT = {};
+        const value = ldclient.variation(flagKey, NO_RESULT);
+        if (value == NO_RESULT) {
+          return;
+        }
+        if (attrName == "text") {
+          elem.innerText = value;
+          return;
+        }
+        if (attrName.startsWith("style")) {
+          const styleProp = attrName.substring(5);
+          console.log(styleProp, value,'style')
+          elem.style[styleProp] = value;
+          return
+        }
+        if (attrName == "class") {
+          const classes = Array.isArray(value) ? value.join(' ') : value;
+          console.log("classes", classes);
+          elem.setAttribute("class", classes)
+          return;
+        }
+        elem.setAttribute(attrName, value)
+      });      
     }
 
     // Dynamically loads the LaunchDarkly browser-JS SDK
     function loadLdJsSDK(url, handler){
+      console.log('I AM IN LOAD')
         var scriptTag = document.createElement('script');
         scriptTag.src = url;
         scriptTag.crossOrigin = "anonymous";
@@ -24,12 +57,32 @@
         scriptTag.onload = handler;
         scriptTag.onreadystatechange = handler;
     
-        document.body.appendChild(scriptTag);    
+        document.head.appendChild(scriptTag);    
     }
 
     // Performs initial flag evaluations and sets up change handlers
     function initSdk() {
+            console.log('I AM LOAD')
 
+      let conf = getConfigAndUserFromMeta();
+      console.log(conf);
+      window.ldclient = LDClient.initialize(conf.clientid, conf.user, conf.config);
+      window.ldclient.on('ready', () => {
+        console.log("INIT")
+        if (document.readyState == "complete") {
+          console.log("HOLA")
+          updateAllElements();
+        } else {
+          document.addEventListener("DOMContentLoaded", updateAllElements)
+        }
+        if(true || conf.config.stream == true) {
+        window.ldclient.on('change',() => {
+          console.log("change");
+          updateAllElements();
+        })
+      }
+      });
+     
     }
 
     function getConfigAndUserFromMeta() {
@@ -45,16 +98,15 @@
             "anonymous",
         ];
 
-        const privateAttributeNames = [];
 
-        let result = { user: { anonymous: true, custom: {} }, config: { pr } };
+        let result = { user: { anonymous: true, custom: {} }, config: { privateAttributeNames: [] } };
         return Array.from(document.querySelectorAll("head meta[property^='ld:']"))
             .map(function (elem) {
                 console.log(elem);
                 const [_, kind, name] = elem.getAttribute("property").split(":", 3);
                 return (
                     (kind &&
-                    name && [
+                    (name || kind == "clientid") && [
                         kind,
                         name,
                         elem.getAttribute("content"),
@@ -69,6 +121,7 @@
                 acc,
                 [kind, name, value, is_private, should_parse_json]
                 ) {
+                  console.log(kind, name, value, is_private, should_parse_json)
                     if (!acc.hasOwnProperty(kind) && kind !== "clientid") {
                         acc[kind] = {};
                     }
@@ -98,9 +151,9 @@
                         }
                     }
                     if (kind == "clientid") {
-                        target = acc;
                         name = "clientid";
-                        delete acc.clientid;
+                        acc.clientid = value;
+                        return acc;
                     }
                     if (target.hasOwnProperty(name)) {
                         target[name] = [target[name], value];
